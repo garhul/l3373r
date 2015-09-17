b2f14ff8e74a394e45e30ae062e5c1cdeb5dcc7a
Garhul <adrian.gesto@gmail.com>
2015-09-17 13:38:04 -0300

    fixed caps

--- l33t3r.js
+++ l33t3r.js
@@ -11,15 +11,15 @@ var matches =       ['a','e','i','g','t','s','z','o'];
 var replacements =  ['4','3','1','6','7','5','2','0'];
 
 var input_text = process.argv[2]||"el otro dia sali de casa en camiseta, eso es ser leet";
-
 var arr = input_text.split('');
+
 for (var x = 0; x < arr.length; x++){
 
   if (arr[x] == ' ' && cfg.removeSpaces)
     arr[x] = '';
 
   if (cfg.randomCaps)
-    arr[x] = (Math.random < cfg.capsOcurrence) ? arr[x]:arr[x].toUpperCase();
+    arr[x] = (Math.random() > cfg.capsOcurrence) ? arr[x]:arr[x].toUpperCase();
 
   arr[x] = (matches.indexOf(arr[x].toLowerCase()) != -1)? replacements[matches.indexOf( arr[x].toLowerCase() )] : arr[x];
 
